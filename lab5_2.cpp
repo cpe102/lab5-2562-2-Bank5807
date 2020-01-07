9813de98cd95e1b7949dcdfc29b3a85c8d09ce28
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-07 15:53:40 +0700

    lab5_2 v1

--- lab5_2.cpp
+++ lab5_2.cpp
@@ -1,17 +1,32 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
 
-
-
-
-int main(){	
-	cout << mixText("AAA","BBB") << "\n";
+string mixText(string A, string B)
+{
+    string y;
+    int i = 0, L1 = A.size(), L2 = B.size();
+    if(L1 == L2)
+    {
+        while(i < L1)
+        {
+        cout << A[i];
+        cout << B[i];
+        i++;
+        }
+        return y;
+    }else{
+        return "E";
+    }
+}
+int main()
+{
+    cout << mixText("AAA","BBB") << "\n";
 	cout << mixText("Hello","World") << "\n";
 	cout << mixText("SOTUS","CHEER") << "\n";
 	cout << mixText("1234","5678") << "\n";
 	cout << mixText("6","9") << "\n";
-	cout << mixText("XXXXXXX","YYY") << "\n";	
-	cout << mixText("Y","XX") << "\n";	
-	
-	return 0;	
+	cout << mixText("XXXXXXX","YYY") << "\n";
+	cout << mixText("Y","XX") << "\n";
+    return 0;
 }
